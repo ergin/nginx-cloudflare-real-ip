ab85fa5e61817c192bcf209c85cf6cad5f040256
ergin <ergin@erginbulut.com>
2022-02-24 19:25:15 +0300

    Issue #7 "CloudFlare redirects (HTTP Status Code: 301) ipv4 and ipv6 links to new URLs" fixed.

--- cloudflare-sync-ips.sh
+++ cloudflare-sync-ips.sh
@@ -6,13 +6,13 @@ echo "#Cloudflare" > $CLOUDFLARE_FILE_PATH;
 echo "" >> $CLOUDFLARE_FILE_PATH;
 
 echo "# - IPv4" >> $CLOUDFLARE_FILE_PATH;
-for i in `curl https://www.cloudflare.com/ips-v4`; do
+for i in `curl -s -L https://www.cloudflare.com/ips-v4`; do
         echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH;
 done
 
 echo "" >> $CLOUDFLARE_FILE_PATH;
 echo "# - IPv6" >> $CLOUDFLARE_FILE_PATH;
-for i in `curl https://www.cloudflare.com/ips-v6`; do
+for i in `curl -s -L https://www.cloudflare.com/ips-v6`; do
         echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH;
 done
 
