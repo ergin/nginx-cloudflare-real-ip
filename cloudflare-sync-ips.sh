f19f7d671ab8db5c869e523db8d60932bf4641e8
blissjoe-gv <blissjoe@gvsu.edu>
2024-02-02 11:17:06 -0500

    add flag for true-client-ip

--- cloudflare-sync-ips.sh
+++ cloudflare-sync-ips.sh
@@ -2,20 +2,25 @@
 
 cf_ips="$(curl -fsLm2 --retry 1 https://api.cloudflare.com/client/v4/ips)"
 CLOUDFLARE_FILE_PATH=${1:-/etc/nginx/cloudflare}
+true_client=false
 
 echo "# Cloudflare IP Ranges" > $CLOUDFLARE_FILE_PATH
 echo "" >> $CLOUDFLARE_FILE_PATH
 echo "# - IPv4" >> $CLOUDFLARE_FILE_PATH
 for ipv4 in $(echo "$cf_ips" | jq -r '.result.ipv4_cidrs[]//""' | sort); do
 	echo "set_real_ip_from $ipv4;" >> $CLOUDFLARE_FILE_PATH
-done
+ done
 echo "" >> $CLOUDFLARE_FILE_PATH
 echo "# - IPv6" >> $CLOUDFLARE_FILE_PATH
 for ipv6 in $(echo "$cf_ips" | jq -r '.result.ipv6_cidrs[]//""' | sort); do
     echo "set_real_ip_from $ipv6;" >> $CLOUDFLARE_FILE_PATH
  done
- echo "" >> $CLOUDFLARE_FILE_PATH
- echo "real_ip_header CF-Connecting-IP;" >> $CLOUDFLARE_FILE_PATH
+echo "" >> $CLOUDFLARE_FILE_PATH
+ if [ $true_client = true ] ; then
+   echo "real_ip_header True-Client-IP;" >> $CLOUDFLARE_FILE_PATH
+ else
+   echo "real_ip_header CF-Connecting-IP;" >> $CLOUDFLARE_FILE_PATH
+ fi
 
 #test configuration and reload nginx
 nginx -t && systemctl reload nginx
